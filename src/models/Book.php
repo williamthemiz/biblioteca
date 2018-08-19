9dace57eef941b8694ee3fb3d9ed2bbe047ed9d8
William Martinez <williamthemiz@gmail.com>
2018-08-18 20:55:13 -0600

    refactored code
    made code more readable and using good standarts

--- src/models/Book.php
+++ src/models/Book.php
@@ -1,15 +1,17 @@
 <?php 
 class Book {
-	private $code;
-	private $title;
-	private $author;
-	private $category;
-	private $stock;
-	private $image;
-	private $pages;
-	private $editorial;
-	private $description;
-	private $publication_date;
+	public $id;
+	public $code;
+	public $title;
+	public $author;
+	public $category;
+	public $stock;
+	public $image;
+	public $pages;
+	public $editorial;
+	public $description;
+	public $publication_date;
+	public $active = 1;
   private $connection;
   
 	function __construct() {
@@ -17,138 +19,171 @@ class Book {
 	}
 
 	function getAll(){
-    $query = "SELECT * FROM libros WHERE activo = 1 AND existencia>0 LIMIT 32";
+    $query = "SELECT * FROM books WHERE active = 1 AND stock > 0 LIMIT 32";
     $statement = $this->connection->prepare($query);
     $statement->execute();
     $books = $statement->fetchAll(PDO::FETCH_ASSOC);
 	  return $books;
 	}
 
-	function get($code){
-		$book = $this->connection->query("SELECT * FROM libros WHERE codigo=$code");
-		if ($book) {
-			return mysqli_fetch_array($book);
-		}
-		return "";
+	function get(){
+		$query = "SELECT * FROM books WHERE id = :id";
+		$statement = $this->connection->prepare($query);
+		$statement->bindParam(':id',$this->id);
+    $statement->execute();
+		$book = $statement->fetch(PDO::FETCH_ASSOC);
+		return $book;
 	}
 
-	function agregarLibro($titulo,$autor,$categoria,$existencia,$imagen,$numPaginas,$editorial,$descripcion,$fechaPublicacion)
-	{
-		$activo = 1;
-	$statement = $this->connection->prepare("INSERT INTO libros (titulo,autor,categoria,existencia,imagen,numPaginas,editorial,descripcion,fechaPublicacion,activo) VALUES (?,?,?,?,?,?,?,?,?,?)");
-
-    $statement->bind_param("ssiisissii",$titulo,$autor,$categoria,$existencia,$imagen,$numPaginas,$editorial,$descripcion,$fechaPublicacion,$activo);  
+	function save(){
+		$query = "INSERT INTO books (code,title,author,category,stock,image,pages,editorial,description,publication_date,active) 
+		VALUES (:code,:title,:author,:category,:stock,:image,:pages,:editorial,:description,:publication_date,:active)";
+		$statement = $this->connection->prepare($query);
+		$statement->bindParam(':code',$this->code,PDO::PARAM_INT);
+		$statement->bindParam(':title',$this->title,PDO::PARAM_STR);
+		$statement->bindParam(':author',$this->author,PDO::PARAM_STR);
+		$statement->bindParam(':category',$this->category,PDO::PARAM_INT);
+		$statement->bindParam(':stock',$this->stock,PDO::PARAM_INT);
+		$statement->bindParam(':image',$this->image,PDO::PARAM_STR);
+		$statement->bindParam(':pages',$this->pages,PDO::PARAM_INT); 
+		$statement->bindParam(':editorial',$this->editorial,PDO::PARAM_STR); 
+		$statement->bindParam(':description',$this->description,PDO::PARAM_STR); 
+		$statement->bindParam(':publication_date',$this->publication_date,PDO::PARAM_STR);
+		$statement->bindParam(':active',$this->active,PDO::PARAM_INT); 
     $resultado = $statement->execute();
     return $resultado;
 	}
 
-	function buscarPorTexto($texto)
-	{
-		$texto = $this->connection->escape_string($texto);
-		$libros =$this->connection->query("SELECT * FROM libros WHERE activo = 1 AND existencia>0 AND ((titulo LIKE '%" . $texto ."%') OR 
-			(codigo LIKE '%" . $texto ."%') OR (autor LIKE '%" . $texto ."%') OR (descripcion LIKE '%" . $texto ."%'))");
-		return $libros;
+	function update(){
+		$query = "UPDATE books SET
+		code = :code,
+		title = :title,
+		autor = :author,
+		category = :category,
+		stock = :stock,
+		image = :image,
+		pages = :pages,
+		editorial = :editorial,
+		description = :description,
+		publication_date = :publication_date WHERE id = :id";
+		$statement = $this->connection->prepare($query);
+		$statement->bindParam(':code',$this->code,PDO::PARAM_INT);
+		$statement->bindParam(':title',$this->title,PDO::PARAM_STR);
+		$statement->bindParam(':author',$this->author,PDO::PARAM_STR);
+		$statement->bindParam(':category',$this->category,PDO::PARAM_INT);
+		$statement->bindParam(':stock',$this->stock,PDO::PARAM_INT);
+		$statement->bindParam(':image',$this->image,PDO::PARAM_STR);
+		$statement->bindParam(':pages',$this->pages,PDO::PARAM_INT); 
+		$statement->bindParam(':editorial',$this->editorial,PDO::PARAM_STR); 
+		$statement->bindParam(':description',$this->description,PDO::PARAM_STR); 
+		$statement->bindParam(':publication_date',$this->publication_date,PDO::PARAM_STR);
+		$statement->bindParam(':active',$this->active,PDO::PARAM_INT); 
+    $resultado = $statement->execute();
+    return $resultado;
 	}
 
-	function buscarPorTitulo($titulo){
-		$r=$this->connection->query("SELECT * FROM libros WHERE (titulo LIKE '%" . $titulo ."%') and activo=1");
-		return $r;
+	function delete(){
+		$query = "UPDATE books SET active = :active WHERE id = :id";
+		$statement = $this->connection->prepare($query);
+		$statement->bindParam(':active',$this->active,PDO::PARAM_INT);
+		$statement->bindParam(':id',$this->id,PDO::PARAM_INT);
+		$result = $statement->execute();
+		return $result;
 	}
 
-	function buscarPorCategoria($categoria)
-	{
-		$libros =$this->connection->query("SELECT * FROM libros WHERE categoria = $categoria AND activo = 1 AND existencia>0");
-		return $libros;
+	function filter($text){
+		$query = "SELECT * FROM books WHERE active = 1 AND stock > 0 AND ((title LIKE :text) OR 
+		(code LIKE :text) OR (author LIKE :text) OR (description LIKE :text))";
+		$statement = $this->connection->prepare($query);
+		$statement->bindParam(':text',"%$texto%",PDO::PARAM_STR);
+		$statement->execute();
+		$books = $statement->fetchAll(PDO::FETCH_ASSOC);
+		return $books;
 	}
 
+	function filterByTitle($title){
+		$query = "SELECT * FROM books WHERE active = 1  AND stock > 0 AND (titulo LIKE :title)";
+		$statement = $this->connection->prepare($query);
+		$statement->bindParam(':title', "%$titulo%",PDO::PARAM_STR);
+		$statement->execute();
+		$books = $statement->fetchAll(PDO::FETCH_ASSOC);
+		return $books;
+	}
+
+	function filterByCategory($categoria){
+		$query = "SELECT * FROM books WHERE category = :category AND active = 1 AND stock > 0";
+		$statement = $this->connection->prepare($query);
+		$statement->bindParam(':title', $categoria,PDO::PARAM_INT);
+		$statement->execute();
+		$books = $statement->fetchAll(PDO::FETCH_ASSOC);
+		return $books;
+	}
+
+/*
 	function buscarPorCampo($campo,$texto)
 	{
 		$texto = $this->connection->escape_string($texto);
-		$libros =$this->connection->query("SELECT * FROM libros WHERE activo = 1 AND existencia>0 AND $campo LIKE '%" . $texto . "%'");
-		return $libros;
+		$books =$this->connection->query("SELECT * FROM books WHERE activo = 1 AND existencia>0 AND $campo LIKE '%" . $texto . "%'");
+		return $books;
 	}
 
 	function buscarPorTextoyCat($categoria,$texto)
 	{
 		$texto = $this->connection->escape_string($texto);
-		$libros =$this->connection->query("SELECT * FROM libros WHERE activo = 1 AND existencia>0 AND categoria = $categoria AND ((titulo LIKE '%" . $texto ."%') OR 
-			(codigo LIKE '%" . $texto ."%') OR (autor LIKE '%" . $texto ."%') OR (descripcion LIKE '%" . $texto ."%'))");
-		return $libros;
+		$books =$this->connection->query("SELECT * FROM books WHERE activo = 1 AND existencia>0 AND categoria = $categoria AND ((titulo LIKE '%" . $texto ."%') OR 
+			(id LIKE '%" . $texto ."%') OR (autor LIKE '%" . $texto ."%') OR (descripcion LIKE '%" . $texto ."%'))");
+		return $books;
 	}
 
 	function buscarCompleto($campo,$categoria,$texto)
 	{
 		$texto = $this->connection->escape_string($texto);
-		$libros =$this->connection->query("SELECT * FROM libros WHERE activo = 1 AND existencia>0 AND categoria = $categoria AND $campo LIKE '%" . $texto . "%'");
-		return $libros;
+		$books =$this->connection->query("SELECT * FROM books WHERE activo = 1 AND existencia>0 AND categoria = $categoria AND $campo LIKE '%" . $texto . "%'");
+		return $books;
+	}
+*/
+	function decreaseStock(){
+		$query = "UPDATE books SET stock = stock - 1 WHERE id = :id";
+		$statement = $this->connection->prepare($query);
+		$statement->bindParam(':id', $this->id,PDO::PARAM_INT);
+		$result = $statement->execute();
+		return $result;
 	}
 
-	function disminuirExistencia($codigo)
-	{
-		$statement = $this->connection->prepare("UPDATE libros SET existencia = existencia-1 WHERE codigo=?");
-		$statement->bind_param("i",$codigo);
-		$resultado = $statement->execute();
-	return $resultado;
+	function increaseStock(){
+		$query = "UPDATE books SET stock = stock + 1 WHERE id = :id";
+		$statement = $this->connection->prepare($query);
+		$statement->bindParam(':id', $this->id,PDO::PARAM_INT);
+		$result = $statement->execute();
+		return $result;
 	}
 
-	function aumentarExistencia($codigo)
-	{
-		$statement = $this->connection->prepare("UPDATE libros SET existencia = existencia+1 WHERE codigo=?");
-		$statement->bind_param("i",$codigo);
-		$resultado = $statement->execute();
-	return $resultado;
+	function sortByStock(){
+		$query = "SELECT * FROM books WHERE active = 1 AND stock > 0 ORDER BY stock LIMIT 10";
+    $statement = $this->connection->prepare($query);
+    $statement->execute();
+    $books = $statement->fetchAll(PDO::FETCH_ASSOC);
+	  return $books;
 	}
 
-	function modificarLibro($codigo,$titulo,$autor,$categoria,$existencia,$imagen,$numPaginas,$editorial,$descripcion,$fechaPublicacion){
-			$statement = $this->connection->prepare("UPDATE libros set 
-			titulo=?,autor=?,categoria=?,existencia=?,imagen=?,numPaginas=?,editorial=?,descripcion=?,fechaPublicacion=? where codigo=?");
-		    $statement->bind_param("ssiisissii",$titulo,$autor,$categoria,$existencia,$imagen,$numPaginas,$editorial,$descripcion,$fechaPublicacion,$codigo);  
-		    $resultado = $statement->execute();
-		    return $resultado;
-	}
-
-	function modificarLibroSinImagen($codigo,$titulo,$autor,$categoria,$existencia,$numPaginas,$editorial,$descripcion,$fechaPublicacion){
-			$statement = $this->connection->prepare("UPDATE libros set 
-			titulo=?,autor=?,categoria=?,existencia=?,numPaginas=?,editorial=?,descripcion=?,fechaPublicacion=? where codigo=?");
-		    $statement->bind_param("ssiiissii",$titulo,$autor,$categoria,$existencia,$numPaginas,$editorial,$descripcion,$fechaPublicacion,$codigo);  
-		    $resultado = $statement->execute();
-		    return $resultado;
-	}
-
-	function mostrarPorExistencia()
-	{
-		$libros=$this->connection->query("SELECT * FROM libros WHERE activo = 1 ORDER BY existencia LIMIT 10");
-
-	return $libros;
-	}
-	
-	function eliminarLibro($codigo){
-		$activo=0;
-		$statement = $this->connection->prepare("UPDATE libros set activo=? where codigo=?");
-		$statement->bind_param("ii",$activo,$codigo);
-		$respuesta=$statement->execute();
-		return $respuesta;
-	}
-
-	 function filtrarLibros($texto) //muestra todos los libros que contienen el texto indicado ordenados por existencias
-    {
-        $statement = $this->connection->prepare("SELECT codigo,titulo,existencia FROM libros WHERE activo = 1 AND ((codigo LIKE ?) OR (titulo LIKE ?) OR (existencia LIKE ?)) ORDER BY existencia LIMIT 10");
-        $texto = "%" . $texto . "%";
-        $statement->bind_param("sss",$texto,$texto,$texto);
-        $statement->execute();
-        $statement->bind_result($codigo,$titulo,$existencia);
-
-        while ($statement->fetch()) 
-        {
-            echo "<tr>";
-            echo "<td>$codigo</td>";
-            echo "<td>$titulo</td>";
-             echo "<td>$existencia</td>";
-           echo "<td>" . '<a href="pedidos.php?l=' . $codigo . '">Agregar a pedido</a>' . "</td>";
-            echo "</tr>";
-        }
-
+	 function filtrarbooks($text){
+		//muestra todos los books que contienen el texto indicado ordenados por existencias
+		$query = "SELECT id,title,stock FROM books WHERE active = 1 AND ((id LIKE :text) 
+		OR (titulo LIKE :text) OR (stock LIKE :text)) ORDER BY stock LIMIT 10";
+		$statement = $this->connection->prepare($query);
+		$statement->bindParam(':text',"%$text%",PDO::PARAM_STR);
+		$statement->execute();
+		$books = $statement->fetchAll(PDO::FETCH_ASSOC);
+	  return $books;
+		/* while ($statement->fetch()) 
+				{
+				echo "<tr>";
+				echo "<td>$id</td>";
+				echo "<td>$titulo</td>";
+				echo "<td>$existencia</td>";
+				echo "<td>" . '<a href="pedidos.php?l=' . $id . '">Agregar a pedido</a>' . "</td>";
+				echo "</tr>";
+			}*/
     }
 }
-
 ?>
--- src/models/Category.php
+++ src/models/Category.php
@@ -1,76 +1,64 @@
 <?php
 class Category {
-	private $id;
-	private $name;
-  private $active;
+	public $id;
+	public $name;
+  public $active = 1;
   private $connection;
 	
 	function __construct(){
 		$this->connection = Connection::connect();
   }
 
-	public function setName($name){
-		$this->name = $name;
-	}
-
-	public function setActive($active){
-		$this->active = $active;
-	}
-
-  public function getId(){
-    return $this->id;
-  }
-
-  public function getName(){
-    return $this->name;
-  }
-
-  public function getActive(){
-    return $this->active;
-  }
-
 	function getAll(){
-    $query = "SELECT * FROM categorias WHERE activo = 1";
+    $query = "SELECT * FROM categories WHERE active = 1";
     $statement = $this->connection->prepare($query);
     $statement->execute();
     $categories = $statement->fetchAll(PDO::FETCH_ASSOC);
 	  return $categories;
 	}
 
-	function get($id){ //cambiar a prepared
-		$category = $this->connection->query("SELECT nombre FROM categorias WHERE id = $id and activo=1");
+	function get(){
+    $query = "SELECT name FROM categories WHERE id = :id and active = 1";
+    $statement = $this->connection->prepare($query);
+    $statement->bindParam(':id',$this->id);
+    $statement->execute();
+    $category = $statement->fetch(PDO::FETCH_ASSOC);
 		return $category;
   }
-  
-	function filterByName($name){
-    $query = "SELECT * FROM categorias WHERE nombre LIKE %:name% AND activo=1";
-    $statement = $this->connection->prepare($query);
-    $statement->bindParam(':name', $name, PDO::PARAM_STR);
-    $statement->execute();
-		$category = $statement->fetch(PDO::FETCH_ASSOC);
-		return $category;
-	}
 
-	function save($name){	
-    $statement = $this->connection->prepare("INSERT INTO categorias (nombre,activo) VALUES (?,?)");
-    $active = 1;
-    $statement->bind_param('si', $name, $active);
+	function save(){
+    $query = "INSERT INTO categories (name,active) VALUES (:name,:active)";
+    $statement = $this->connection->prepare($query);
+    $statement->bindParam(':name', $this->name, PDO::PARAM_STR);
+    $statement->bindParam(':active', $this->active, PDO::PARAM_INT);
     $result = $statement->execute();
     return $result;
   }
   
-	function update($id,$name){
-		$statement=$this->connection->prepare("UPDATE categorias SET nombre=? WHERE id=?");
-    $statement->bind_param("si",$name,$id);
+	function update(){
+    $query = "UPDATE categories SET name = :name WHERE id = :id";
+		$statement = $this->connection->prepare($query);
+    $statement->bindParam(':name',$this->name);
+    $statement->bindParam(':id',$this->id);
     $result=$statement->execute();
     return $result;
 	}
-	function delete($id){	
-    $statement=$this->connection->prepare("UPDATE categorias SET activo = ? WHERE id = ?");
-    $active=0;
-    $statement->bind_param("ii",$active,$id);
+	function delete(){
+    $query = "UPDATE categories SET active = :active WHERE id = :id";
+    $statement=$this->connection->prepare();
+    $statement->bindParam(':active',$this->active);
+    $statement->bindParam(':id',$this->id);
     $result = $statement->execute();
     return $result;
+  }
+  
+  function filterByName(){
+    $query = "SELECT * FROM categories WHERE name LIKE :name AND active = 1";
+    $statement = $this->connection->prepare($query);
+    $statement->bindParam(':name', "%" . $this->name . "%", PDO::PARAM_STR);
+    $statement->execute();
+		$category = $statement->fetch(PDO::FETCH_ASSOC);
+		return $category;
 	}
 }
 ?>
